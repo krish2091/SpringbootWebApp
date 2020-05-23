064f8b6cd3ab142203397e98a083f3dfd0dd0b6e
Krishna <Krishna@192.168.0.12>
2020-05-23 19:09:08 +0530

    First Commit

---
+++ SpringbootWebApp/src/main/java/com/sbw/SpringbootWebAppApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Author RK
+ */
+package com.sbw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebAppApplication.class, args);
+	}
+
+}
